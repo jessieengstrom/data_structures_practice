c448738696fa3ab292f5bc5ace828c4c71fa0091
Jessie Engstrom <jessielengstrom@gmail.com>
2018-06-01 12:13:29 -0700

    remove duplicates froma linked list

--- linkedlist.py
+++ linkedlist.py
@@ -34,16 +34,48 @@ class List(object):
 
         while fast != None:
             slow = slow.next_node
-
             if fast.next_node != None:
                 fast = fast.next_node.next_node
             else:
                 return False
-
             if slow is fast:
                 return True
-
         return False
+
+    def insert_end(self, data):
+        """Insert a node at the end of the list."""
+
+        current = self.head
+        while current.next:
+            current = current.next_node
+
+        current.next_node = Node(data)
+
+    def insert_begin(self, data):
+        """Insert a node at the begining."""
+
+        current = self.head
+        new_node = Node(data)
+        new_node.next_node = current
+        self.head = new_node
+
+    def delete_dups(self):
+        """Remove duplicates in a linked list."""
+
+        slow = self.head
+        fast = slow.next_node
+
+        while fast:
+
+            if slow.data == fast.data:
+                slow.next_node = fast.next_node
+
+            if fast.next_node != None:
+                slow = slow.next_node
+                fast = slow.next_node
+            else:
+                break
+
 
 
 class Node(object):
@@ -52,4 +84,20 @@ class Node(object):
     def __init__(self, data):
         self.data = data
         self.next_node = None
+
+
+one = Node(1)
+two = Node(2)
+three = Node(3)
+three2 = Node(3)
+
+one.next_node = three
+three.next_node = three2
+three2.next_node = two
+
+lst = List(one)
+
+
+
+
 
