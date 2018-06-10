c02a404e2f98ad7cb0846e621f009ffe0a882872
Jessie Engstrom <jessielengstrom@gmail.com>
2018-06-10 13:08:42 -0700

    added insert and del min to the min heap

--- min_heap.py
+++ min_heap.py
@@ -21,7 +21,7 @@ class MinHeap(object):
         """Insert a value into a binary heap."""
 
         self.min_heap.append(val)
-        self.current_size += 1
+        self.current_size = self.current_size + 1
         self.perc_up(self.current_size)
 
         return self.min_heap
@@ -29,13 +29,32 @@ class MinHeap(object):
     def perc_down(self, i):
         """Move a value down so the correct position"""
 
+        while (i * 2) < self.current_size:
+            min_child = self.min_child(i)
+            if self.min_heap[i] > self.min_heap[min_child]:
+                parent = self.min_heap[i]
+                self.min_heap[i] = self.min_heap[min_child]
+                self.min_heap[min_child] = parent
+
+            i = min_child
+
+    def min_child(self, i):
+        """gets the current min child."""
+        if i * 2 > self.current_size:
+            return i * 2
+        else:
+            if self.min_heap[i * 2] > self.min_heap[i * 2 + 1]:
+                return i * 2 + 1
+            else:
+                return i * 2
+
     def del_min(self):
         """Dequeue the minimium node."""
 
         min = self.min_heap[1]
         self.min_heap[1] = self.min_heap[self.current_size]
-        self.current_size -= 1
+        self.current_size = self.current_size - 1
         self.min_heap.pop()
         self.perc_down(1)
 
-        return min
+        return self.min_heap
